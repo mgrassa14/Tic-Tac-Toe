9f904534fe0b668f0006d4aa5eb28e516622b37f
mgrassa0014 <mgrassa00@gmail.com>
2023-03-09 11:45:59 -0800

    update some code

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-console.log("JS TEST MAIN BRANCH!")
+// console.log("JS TEST MAIN BRANCH!")
 // const getWinner = [
 //     {A1:0, B1:0, C1:0},
 //     {A2:0, B2:0, C2:0},
@@ -12,11 +12,6 @@ console.log("JS TEST MAIN BRANCH!")
 
 let board = {A1:0, B1:0, C1:0, A2:0, B2:0, C2:0,A3:0, B3:0, C3:0}
 
-
-
-
-
-
 // let keys = Object.forEach.keys(getWinner).
 // console.log(keys)
 // X's = -1
@@ -28,6 +23,8 @@ let oPieces = 4
 let moves = 0
 
 let player = document.querySelector('.players-turn');
+const xPiecesLeft = document.querySelector('.piecesX')
+const oPiecesLeft = document.querySelector('.piecesO')
 let buttons = document.querySelectorAll('.box');
 const boardEl = document.querySelector('.board')
 
@@ -44,28 +41,33 @@ function gamePlay(e){
     console.log('returning')
     return 
   }
+  
+  // console.log(document.getElementsByClassName('piecesX'),'<<<<')
   let key = e.target.className.split(' ')[0]
   // e.target isd the element that the user is click on
   // console.log(e.target.className.split(' ')[0])
-  console.log(window.getComputedStyle(button).cursor, 'check what this is')
+  // console.log(window.getComputedStyle(button).cursor, 'check what this is')
+  // console.log(document.querySelector('.piecesX').innerHTML)
   moves += 1
       // console.log(moves,'<< moves update')
   if (XO === 1){
     XO = -1
     xPieces -= 1
+    xPiecesLeft.textContent = `${xPieces} Pieces Left`;
     player.textContent = "O's Turn"
     e.target.innerHTML = '<img src="https://static.vecteezy.com/system/resources/thumbnails/009/344/496/small/x-transparent-free-png.png"/>'
     e.target.style.cursor = 'not-allowed';
   } else {
     XO = 1
     oPieces -= 1
+    oPiecesLeft.textContent = `${oPieces} Pieces Left`;
     player.textContent = "X's Turn"
     e.target.innerHTML = '<img src="https://i.imgur.com/mrTYNoE.png"/>'
     e.target.style.cursor = 'not-allowed';
   } 
   // console.log(XO,'<< XO')
   board[key] = XO
-  // console.log(board, '<<')
+  // // console.log(board, '<<')
   // console.log(xPieces, 'Pieces left X')
   // console.log(oPieces, 'Pieces left O')
   // console.log(e.target,'blob')
@@ -75,15 +77,17 @@ function gamePlay(e){
 }
 
 
-
-
-
 function getWinner(){
-  console.log(moves,'<<moves')
+  // console.log(moves,'<<moves')
   // console.log(board, '>>>')
   // console.log(board.A1)
+  console.log(player.textContent)
+  if (player.textContent === "X Wins!" || player.textContent === "O Wins!"){
+    return
+  } 
   if ((board.A1 + board.A2 + board.A3) === -3 || (board.B1 + board.B2 + board.B3) === -3 || (board.C1 + board.C2 + board.C3) === -3 || (board.A1 + board.B1 + board.C1) === -3 || (board.A2 + board.B2 + board.C2) === -3 || (board.A3 + board.B3 + board.C3) === -3 || (board.A1 + board.B2 + board.C3) === -3 || (board.C1 + board.B2 + board.A3) === -3){
     player.textContent = "X Wins!"
+    // e.target.style.background-color(red); 
     boardEl.removeEventListener('click', function gamePlay(){})
     // document.getElementsByClassName('A1').style
   } else if ((board.A1 + board.A2 + board.A3) === 3 || (board.B1 + board.B2 + board.B3) === 3 || (board.C1 + board.C2 + board.C3) === 3 || (board.A1 + board.B1 + board.C1) === 3 || (board.A2 + board.B2 + board.C2) === 3 || (board.A3 + board.B3 + board.C3) === 3 || (board.A1 + board.B2 + board.C3) === 3 || (board.C1 + board.B2 + board.A3) === 3){
@@ -92,8 +96,30 @@ function getWinner(){
   } else if (moves === 9 && ((board.A1 + board.A2 + board.A3) < 3 || (board.B1 + board.B2 + board.B3) < 3 || (board.C1 + board.C2 + board.C3) < 3 || (board.A1 + board.B1 + board.C1) < 3 || (board.A2 + board.B2 + board.C2) < 3 || (board.A3 + board.B3 + board.C3) < 3 || (board.A1 + board.B2 + board.C3) < 3 || (board.C1 + board.B2 + board.A3) < 3)){
     player.textContent = "Cats Game!"
   }
-  // boardEl.removeEventListener('click', function(){})
+  
 }
+
+
+let resetEl = document.querySelector('.reset')
+resetEl.addEventListener('click', function(){
+  XO = 1
+  xPieces = 5
+  oPieces = 4
+  moves = 0
+  buttons.forEach(element => element.innerHTML = '')
+  player.textContent = "X's Turn"
+  xPiecesLeft.textContent = '5 Pieces Left';
+  oPiecesLeft.textContent = '4 Pieces Left';
+  board = {A1:0, B1:0, C1:0, A2:0, B2:0, C2:0,A3:0, B3:0, C3:0}
+  buttons.forEach(element => element.style.cursor = '' );
+
+  gamePlay()
+})
+
+
+
+
+
 
 // function gamePlay(){
 //   for(let i = 0; i < buttons.length; i++){
@@ -125,23 +151,3 @@ function getWinner(){
 // }
 
 // gamePlay()
-
-let resetEl = document.querySelector('.reset')
-resetEl.addEventListener('click', function(e){
-  XO = 1
-  xPieces = 5
-  oPieces = 4
-  moves = 0
-  buttons.forEach(element => element.innerHTML = '')
-  player.textContent = "X's Turn"
-  board = {A1:0, B1:0, C1:0, A2:0, B2:0, C2:0,A3:0, B3:0, C3:0}
-  buttons.forEach(element => element.style.cursor = '' );
-  // console.log(board)
-  // remove event listeners
-  // for(let i = 0; i < buttons.length; i++){
-  //   buttons[i].removeEventListener('click', function(){})
-  // }
-  // boardEl.removeEventListener('click', function(){})
-  // gameplay function 
-  gamePlay()
-})

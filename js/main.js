ce888841d0be4bac05462f35e302c7d84ac83a00
mgrassa0014 <mgrassa00@gmail.com>
2023-03-09 19:02:25 -0800

    Add comments/pseudocode to js

--- js/main.js
+++ js/main.js
@@ -1,156 +1,138 @@
-// console.log("JS TEST MAIN BRANCH!")
-// const getWinner = [
-//     {A1:0, B1:0, C1:0},
-//     {A2:0, B2:0, C2:0},
-//     {A3:0, B3:0, C3:0},
-//     {A1:0, A2:0, A3:0},
-//     {B1:0, B2:0, B3:0},
-//     {C1:0, C2:0, C3:0},
-//     {A1:0, B2:0, C3:0},
-//     {C1:0, B2:0, A3:0}
-// ]
+// board = {box1 box2 box3 box4 box5 box6 box7 box8 box9}
+let board = {A1:0, B1:0, C1:0, A2:0, B2:0, C2:0,A3:0, B3:0, C3:0};
+// variable to determine who goes first with the values 1 and -1
+let XO = 1;
+// variable for the number of X pieces
+let xPieces = 5;
+// variable for the number of O pieces
+let oPieces = 4;
+// total number of moves/clicks
+let moves = 0;
 
-let board = {A1:0, B1:0, C1:0, A2:0, B2:0, C2:0,A3:0, B3:0, C3:0}
-
-// let keys = Object.forEach.keys(getWinner).
-// console.log(keys)
-// X's = -1
-// O's = 1
-// X will always start first
-let XO = 1
-let xPieces = 5
-let oPieces = 4
-let moves = 0
-
+// variable = html for who's turn it is
 let player = document.querySelector('.players-turn');
-const xPiecesLeft = document.querySelector('.piecesX')
-const oPiecesLeft = document.querySelector('.piecesO')
-let buttons = document.querySelectorAll('.box');
+// variable = html for the number of x pieces left
+const xPiecesLeft = document.querySelector('.piecesX');
+// variable = html for the number of O pieces
+const oPiecesLeft = document.querySelector('.piecesO');
+// variable = html for all of my buttons with a class of "box"
+const buttons = document.querySelectorAll('.box');
+// variable = html for the class of "board"
 const boardEl = document.querySelector('.board')
 
-// let classes = document.getElementsById('box').classList.items(0);
-// console.log(buttons[0].attr('class').split(' ').length);
-// console.log(classes)
-
-// const boardEl = document.querySelector('.board')
-
+// click event listener to boardEl for all of my buttons to call function gameplay
 boardEl.addEventListener('click', gamePlay)
+// create funciton gameplay to start the game with a parameter "e" that represents every click the user does
 function gamePlay(e){
-  const button = e.target
+  // declare variable = button clicked html
+  const button = e.target;
+  // if the css cerser = "not-allowed", do not continue through the function 
   if (window.getComputedStyle(button).cursor === 'not-allowed'){
-    console.log('returning')
-    return 
-  }
+    return
+  };
+  // if the text content for ".players-turn" = "X Wins!" or "O wins!", do not continue through the function
   if (player.textContent === "X Wins!" || player.textContent === "O Wins!"){
     return
-  } 
-  
-  // console.log(document.getElementsByClassName('piecesX'),'<<<<')
-  let key = e.target.className.split(' ')[0]
-  // e.target isd the element that the user is click on
-  // console.log(e.target.className.split(' ')[0])
-  // console.log(window.getComputedStyle(button).cursor, 'check what this is')
-  // console.log(document.querySelector('.piecesX').innerHTML)
-  moves += 1
-      // console.log(moves,'<< moves update')
+  };
+  // declare variable = the first class within the html element that the user clicks
+  let key = e.target.className.split(' ')[0];
+  // increase number of moves by 1
+  moves += 1;
+  // if the starting value of XO = 1, follow instructions
   if (XO === 1){
-    XO = -1
-    xPieces -= 1
+    // change value of XO = -1
+    XO = -1;
+    // decrease number of x pices by 1
+    xPieces -= 1;
+    // change the the text content of x pieces left to the degreaced value of xPieces 
     xPiecesLeft.textContent = `${xPieces} Pieces Left`;
-    player.textContent = "O's Turn"
-    e.target.innerHTML = '<img src="https://static.vecteezy.com/system/resources/thumbnails/009/344/496/small/x-transparent-free-png.png"/>'
+    // change display to "O's Turn"
+    player.textContent = "O's Turn";
+    // add X image 
+    e.target.innerHTML = '<img src="https://static.vecteezy.com/system/resources/thumbnails/009/344/496/small/x-transparent-free-png.png"/>';
+    // after clicking it once, user can no longer click on button
     e.target.style.cursor = 'not-allowed';
+  // if the starting value of XO != 1, follow instructions 
   } else {
-    XO = 1
-    oPieces -= 1
+    // change value of XO = 1
+    XO = 1;
+    // decrease number of O pices by 1
+    oPieces -= 1;
+    // change the text content of O pieces left to the decreased value of oPieces
     oPiecesLeft.textContent = `${oPieces} Pieces Left`;
-    player.textContent = "X's Turn"
-    e.target.innerHTML = '<img src="https://i.imgur.com/mrTYNoE.png"/>'
+    // change display to "X's Turn"
+    player.textContent = "X's Turn";
+    // add O image
+    e.target.innerHTML = '<img src="https://i.imgur.com/mrTYNoE.png"/>';
+    // after clicking it once, user can no longer click on button
     e.target.style.cursor = 'not-allowed';
-  } 
-  // console.log(XO,'<< XO')
-  board[key] = XO
-  // // console.log(board, '<<')
-  // console.log(xPieces, 'Pieces left X')
-  // console.log(oPieces, 'Pieces left O')
-  // console.log(e.target,'blob')
-  // e.target.removeEventListener('click', gamePlay)
-
-  getWinner()
+  };
+  // add the changed value of XO after click to board object key that has the same class name
+  board[key] = XO;
+  // call getWinner function
+  getWinner();
 }
 
-
+// create getWinner function to determine the winner of the game and change the background colors
 function getWinner(){
-  // console.log(moves,'<<moves')
-  // console.log(board, '>>>')
-  // console.log(board.A1)
-  console.log(player.textContent)
-  // if (player.textContent === "X Wins!" || player.textContent === "O Wins!"){
-  //   return
-  // } 
+  // if any of the winning possibilities sum = -3, follow instructions
   if ((board.A1 + board.A2 + board.A3) === -3 || (board.B1 + board.B2 + board.B3) === -3 || (board.C1 + board.C2 + board.C3) === -3 || (board.A1 + board.B1 + board.C1) === -3 || (board.A2 + board.B2 + board.C2) === -3 || (board.A3 + board.B3 + board.C3) === -3 || (board.A1 + board.B2 + board.C3) === -3 || (board.C1 + board.B2 + board.A3) === -3){
-    player.textContent = "X Wins!"
-    // e.target.style.background-color(red); 
-    boardEl.removeEventListener('click', function gamePlay(){})
-    // document.getElementsByClassName('A1').style
+    // for each element within buttons(which has a list of all of my html buttons) change to the color red
+    buttons.forEach(element => {
+      element.style.backgroundColor = 'red';
+    });
+    // change the text content of ".players-turn" class "X Wins!"
+    player.textContent = "X Wins!";
+    // remove the event listener for the board, which also removes the event listener click from the buttons
+    boardEl.removeEventListener('click', function gamePlay(){});
+  // if any of the winning possibilities sum = 3, follow instructions  
   } else if ((board.A1 + board.A2 + board.A3) === 3 || (board.B1 + board.B2 + board.B3) === 3 || (board.C1 + board.C2 + board.C3) === 3 || (board.A1 + board.B1 + board.C1) === 3 || (board.A2 + board.B2 + board.C2) === 3 || (board.A3 + board.B3 + board.C3) === 3 || (board.A1 + board.B2 + board.C3) === 3 || (board.C1 + board.B2 + board.A3) === 3){
-    player.textContent = "O Wins!"
-    boardEl.removeEventListener('click', function gamePlay(){})
+    // for each element within buttons(which has a list of all of my html buttons) change to the color blue
+    buttons.forEach(element => {
+      element.style.backgroundColor = 'blue';
+    });
+    // change the text content of ".players-turn" class "O Wins!"
+    player.textContent = "O Wins!";
+    // remove the event listener for the board, which also removes the event listener click from the buttons
+    boardEl.removeEventListener('click', function gamePlay(){});
+  // if the total moves = 9 and any of the winning posibilites sum is less than 3, follow isntructions 
   } else if (moves === 9 && ((board.A1 + board.A2 + board.A3) < 3 || (board.B1 + board.B2 + board.B3) < 3 || (board.C1 + board.C2 + board.C3) < 3 || (board.A1 + board.B1 + board.C1) < 3 || (board.A2 + board.B2 + board.C2) < 3 || (board.A3 + board.B3 + board.C3) < 3 || (board.A1 + board.B2 + board.C3) < 3 || (board.C1 + board.B2 + board.A3) < 3)){
-    player.textContent = "Cats Game!"
-  }
+    // for each element within buttons(which has a list of all of my html buttons) change to the color purple
+    buttons.forEach(element => {
+      element.style.backgroundColor = 'purple';
+    });
+    // change the text content of ".players-turn" class "Cat's Game!"
+    player.textContent = "Cats Game!";
+  };
+};
 
-}
-
-
-let resetEl = document.querySelector('.reset')
+// set variable = html with classname "reset"
+let resetEl = document.querySelector('.reset');
+// add click event listener to my reset button with a function when clicked
 resetEl.addEventListener('click', function(){
-  XO = 1
-  xPieces = 5
-  oPieces = 4
-  moves = 0
-  buttons.forEach(element => element.innerHTML = '')
-  player.textContent = "X's Turn"
+  // change XO back to starting value
+  XO = 1;
+  // change xPiece to the starting value
+  xPieces = 5;
+  // change oPieces to the starting value
+  oPieces = 4;
+  // reset the value of total moves 
+  moves = 0;
+  // for each element within buttons(which is a list of all of my buttons), replace the inner html with nothing
+  buttons.forEach(element => element.innerHTML = '');
+  // change .player-turn back to original text content
+  player.textContent = "X's Turn";
+  // change the xPiecesLeft and oPiecesLeft text content to starting string
   xPiecesLeft.textContent = '5 Pieces Left';
   oPiecesLeft.textContent = '4 Pieces Left';
-  board = {A1:0, B1:0, C1:0, A2:0, B2:0, C2:0,A3:0, B3:0, C3:0}
+  // set values of board object back to 0's
+  board = {A1:0, B1:0, C1:0, A2:0, B2:0, C2:0,A3:0, B3:0, C3:0};
+  // remove the cursor css 'not-allowed' to allow the user to click the buttons again
   buttons.forEach(element => element.style.cursor = '' );
-
-  gamePlay()
+  // for each element within buttons(which is a list of all of my buttons), reset the background color to no color
+  buttons.forEach(element => {
+    element.style.backgroundColor = '';
+  });
+  // call function gameplay to continue the cycle of playing the tic-tac-toe game
+  gamePlay();
 })
-
-
-
-
-
-
-// function gamePlay(){
-//   for(let i = 0; i < buttons.length; i++){
-//     buttons[i].addEventListener('click', function(){
-//       moves += 1
-//       // console.log(moves,'<< moves update')
-//       if (XO === 1){
-//         boardEl.addEventListener('click', function(e){
-//           // if (e.target.className.split(' ')[0] === )
-//           let key = e.target.className.split(' ')[0]
-//           board[key] = 1
-//           console.log(board)
-//           XO = -1
-//           player.textContent = "O's Turn"
-//           buttons[i].innerHTML = '<img src="https://static.vecteezy.com/system/resources/thumbnails/009/344/496/small/x-transparent-free-png.png"/>'
-//         })
-//       } else {
-//         boardEl.addEventListener('click', function(e){
-//           let key = e.target.className.split(' ')[0]
-//           board[key] = -1
-//           XO = 1
-//           player.textContent = "X's Turn"
-//           buttons[i].innerHTML = '<img src="https://i.imgur.com/mrTYNoE.png"/>'
-//         })
-//       } 
-//       console.log(XO,'<< XO')
-//     },{once : true});
-//   }
-// }
-
-// gamePlay()
